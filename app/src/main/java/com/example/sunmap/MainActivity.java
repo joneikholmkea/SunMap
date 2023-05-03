7b5925ba76a116577f87f8d642d4294099216c01
joneikholmkea <jone@kea.dk>
2023-05-03 11:05:40 +0200

    location

--- app/src/main/java/com/example/sunmap/MainActivity.java
+++ app/src/main/java/com/example/sunmap/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.sunmap;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -13,6 +21,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private MapView mapView;
     private GoogleMap googleMap;
+    private LocationManager manager; // will start/stop the listening
+    private LocationListener listener; // will handle new location events
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,28 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+        manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        listener = location -> {
+            Log.i("myLocation", "new location " + location);
+        };
+        if(ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }else{
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    2000, 10, listener);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int code, @NonNull String[] permissions, @NonNull int[] res) {
+        super.onRequestPermissionsResult(code, permissions, res);
+        if(ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED) {
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    2000, 10, listener);
+        }
     }
 
     @Override
